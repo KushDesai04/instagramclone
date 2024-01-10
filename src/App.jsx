277e72cf75ecda1e96b0d635e22b905fa202d659
Kush Desai <kush.desai@yahoo.co.nz>
2024-01-10 12:29:50 +1000

    added sidebar page and finished auth page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 
+import PageLayout from './Layouts/PageLayouts/PageLayout'
 import AuthPage from './pages/AuthPage/AuthPage'
 import HomePage from './pages/HomePage/HomePage'
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+
 function App() {
 
   return (
     <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/auth' element={<AuthPage />}/>
       </Routes>
+    </PageLayout> {/* Fix the closing tag */}
     </>
   )
 }
